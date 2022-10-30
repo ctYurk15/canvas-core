5350093b0e037b2626f0a257e7c6ad973ae28772
ctYurk15 <levgenetic@gmail.com>
2022-10-30 15:36:37 +0200

    Add random color function & canvas initialization

--- src/functions.js
+++ src/functions.js
@@ -5,6 +5,15 @@ export function getRandomInt(min, max)
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+export function getRandomColor()
+{
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
+
+    return 'rgb('+red+', '+green+', '+blue+')';
+}
+
 export function getEmpty2dArray(width, height)
 {
     let result = [];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import {Engine} from './Engine';
-import {getRandomInt} from './functions';
+import {Engine} from './components/Engine';
+import {Initializer} from './components/Initializer';
+import {getRandomInt, getRandomColor} from './functions';
 import {Rectangle} from './gameobjects/Rectangle';
 import {} from './game-config';
 
 let animation_id = null;
 
-//configuring canvas
-const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const initializer = new Initializer();
+const canvas = initializer.initializeCanvas();
 
 const engine = new Engine(canvas);
-const rect1 = new Rectangle(getRandomInt(100, 150), getRandomInt(100, 150), getRandomInt(50, 150), getRandomInt(50, 150), 'blue');
+const rect1 = new Rectangle(getRandomInt(100, 150), getRandomInt(100, 150), getRandomInt(50, 150), getRandomInt(50, 150), getRandomColor());
 
 engine.addObject(rect1);
 engine.start();
